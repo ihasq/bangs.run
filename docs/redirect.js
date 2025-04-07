24a5dfe8e37e17c1cec1d041255e0b639d48c5e9
ihasq <gsd79d2mtizifb7cv8d119a9xpq3ja@gmail.com>
2025-04-07 14:11:13 +0900

    wip

--- docs/redirect.js
+++
@@ -1,6 +0,0 @@
-if(!localStorage.getItem('/var/hasItem')) {
-	await fetch("./bangs.json").then(res => res.json()).then(json => json.forEach(([p, q]) => localStorage.setItem(`/token/${p}`, q)))
-	localStorage.setItem('/var/hasItem', Date.now())
-}
-const [p, ...q] = location.hash.slice(1).split(/%20+/);
-location.href = location.hash ? localStorage.getItem(`/token/${p}`)?.replace?.("{{{s}}}", q.join(" ")) || "/about" : "/about"
