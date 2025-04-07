a4046c4ab691f4a075d295b58197c3c3110c3c9b
ihasq <gsd79d2mtizifb7cv8d119a9xpq3ja@gmail.com>
2025-04-08 00:32:08 +0900

    wip

--- docs/settings/main.js
+++ docs/settings/main.js
@@ -1,25 +1,55 @@
-import { $, h as html, on } from "https://esm.sh/libh";
+import { $, h as html, on, css } from "https://esm.sh/libh";
 
 export default () => {
 
 	const
-		index = $(0),
-		list = ["a", "b", "c", "d", "e"],
-		page = index.into(x => list[x % list.length]),
-
-		searchQuery = $("")
+		searchQuery = $(""),
+		isBangValid = searchQuery.match(/\s/).into($ => !!$).or(searchQuery.length.into($ => $ == 0)),
+		resultQuery = searchQuery.into($ => localStorage.getItem(`/token/${$}`) || ""),
+		resultEdit = resultQuery.into($ => $),
+		hasChange = resultEdit.into($ => resultQuery.$ !== $)
 	;
 
-	searchQuery.watch(query => {
-
-	});
-
 	return html`
+
 		<h1>Settings</h1>
-		<h2>Search bangs</h2>
-		<input ${{ type: "text", value: searchQuery }}>
-		${html`<label>ok${searchQuery}lob</label>`}
-		<button ${{ [on.click]: () => index.$++ }}>+</button>
-		<button ${{ [on.click]: () => index.$-- }}>-</button>
+		
+		<label>Search Bangs: <input ${{
+			[css.padding]: "4px",
+			type: "text",
+			value: searchQuery,
+			autofocus: true
+		}}></label><br>
+
+		<input ${{
+			value: resultEdit,
+			placeholder: "Not found",
+			[css]: {
+				padding: "4px",
+				width: "50%"
+			}
+		}}>
+		<button ${{
+			disabled: isBangValid.and(hasChange),
+			[css.padding]: "4px",
+			[on.click]() {
+				localStorage.setItem(`/token/${searchQuery.$}`, resultEdit.$)
+			}
+		}}>
+			${resultQuery.into($ => $ == "" ? "create" : "save")}
+		</button>
+
+		<button ${{
+			disabled: resultQuery.into($ => $ == ""),
+			[css.padding]: "4px",
+			[on.click]() {
+				localStorage.setItem(`/token/${searchQuery.$}`, "")
+				resultQuery.$ = "";
+			}
+		}}>
+			reset
+		</button>
+
 	`;
 }
+// $(localStorage).getItem($`/token/${searchQuery}`).suspense("Not found")
