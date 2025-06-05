4e3031ffad304345b61b575c511d42dbaedde75c
ihasq <ihasq@ihasq.com>
2025-06-05 10:25:51 +0900

    wip

--- docs/main.js
+++ docs/main.js
@@ -1,4 +1,4 @@
 if(localStorage.getItem('/var/hasItem')) {
 	const [p, ...q] = location.hash.slice(1).split(/%20+/);
-	location.href = location.hash ? localStorage.getItem(`/token/${p}`)?.replace?.("\0", q.join(" ")) || "/about" : "/about"
+	location.href = location.hash ? localStorage.getItem(`/token/${p}`)?.replace?.("%s", q.join(" ")) || "/about" : "/about"
 }
